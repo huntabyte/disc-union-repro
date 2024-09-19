6ebe7e3068146fbff3480a5bd37eef2a79b7c91d
Hunter Johnston <johnstonhuntera@gmail.com>
2024-09-18 20:19:39 -0400

    add more examples

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,17 @@
 	import Accordion from '$lib/accordion.svelte';
 
 	let value = $state('a single value');
+	let multiValue = $state(['a', 'b', 'c']);
 </script>
 
+<!-- This is unhappy with the type and it shouldnt be -->
 <Accordion type="single" bind:value />
+
+<!-- This is expected to be unhappy with the type and it is -->
+<Accordion type="single" value={multiValue} />
+
+<!-- This is working as expected -->
+<Accordion type="multiple" value={multiValue} />
+
+<!-- This is also working as expected -->
+<Accordion type="single" {value} />
